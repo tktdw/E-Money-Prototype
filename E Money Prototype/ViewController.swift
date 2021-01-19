5080bda36e985936588637c0626948cdfc82223c
Mu'Alim <muhammad.mualim@hotmail.com>
2021-01-19 13:37:15 +0700

    Initial Commit

---
+++ E Money Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E Money Prototype
+//
+//  Created by Muhammad Mu'Alim on 19/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
